8dd171c0b5bdaab26c4fe978edd69e95c8086a6f
CBE7F1F65 <CBE7F1F65@1545aaa9-8966-c3c3-789a-941f51dc20b7>
2011-05-18 04:39:59 +0000

    增加IPhoneProject和相关资源

---
+++ Game_Xcode/ios/Game_XcodeAppController.h
@@ -0,0 +1,14 @@
+//
+//  Game_XcodeAppController.h
+//  Game_Xcode
+//
+//  Created by h5nc on 11-5-18.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+@interface Game_XcodeAppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@end
+
---
+++ Game_Xcode/ios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Game_Xcode
+//
+//  Created by h5nc on 11-5-18.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"Game_XcodeAppController");
+	[pool release];
+	return retVal;
+}
