622feb2f9416f67459b01c9f6ec9313221b145d4
CBE7F1F65 <CBE7F1F65@1545aaa9-8966-c3c3-789a-941f51dc20b7>
2011-05-19 22:07:29 +0000

    增加Iphone旋转方向的View

--- Game_Xcode/ios/Game_XcodeAppController.h
+++ Game_Xcode/ios/Game_XcodeAppController.h
@@ -6,8 +6,13 @@
 //  Copyright __MyCompanyName__ 2011. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
 @interface Game_XcodeAppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
     UIWindow *window;
+    RootViewController	*viewController;
 }
 
 @end
--- Game_Xcode/ios/Game_XcodeAppController.mm
+++ Game_Xcode/ios/Game_XcodeAppController.mm
@@ -10,6 +10,7 @@
 #import "cocos2d.h"
 #import "EAGLView.h"
 #import "AppDelegate.h"
+#import "RootViewController.h"
 
 @implementation Game_XcodeAppController
 
@@ -25,6 +26,11 @@ static AppDelegate s_sharedApplication;
 
     // Add the view controller's view to the window and display.
     window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
+    
+    // Init the View Controller
+	viewController = [[RootViewController alloc] initWithNibName:nil bundle:nil];
+	viewController.wantsFullScreenLayout = YES;
+	
     EAGLView *__glView = [EAGLView viewWithFrame: [window bounds]
                                      pixelFormat: kEAGLColorFormatRGBA8
                                      depthFormat: GL_DEPTH_COMPONENT16_OES
@@ -32,10 +38,16 @@ static AppDelegate s_sharedApplication;
                                       sharegroup: nil
 								   multiSampling: NO
 								 numberOfSamples:0 ];
-    [window addSubview: __glView];
+    [__glView setMultipleTouchEnabled:YES];
+	
+    // make the OpenGLView a child of the view controller
+	[viewController setView:__glView];
+	
+	// make the View Controller a child of the main window
+	[window addSubview: viewController.view];
     [window makeKeyAndVisible];
 
-    [[UIApplication sharedApplication] setStatusBarHidden: YES];
+    [[UIApplication sharedApplication] setStatusBarHidden: true];
     
     cocos2d::CCApplication::sharedApplication().run();
     return YES;
